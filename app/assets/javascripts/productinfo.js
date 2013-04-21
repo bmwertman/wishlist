2fbd6c0bd8ad8c75fcd6c74744de94a76c89d762
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-21 16:58:24 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -1,18 +1,25 @@
-function productInfo() {
-function targetImg() {document.getElementById().src}
-function targetTitle() {document.getElementById().value}
-function targetPrice() {document.getElementsByClassName().value}
-  if (document(location.hostname) == 'www.amazon.com') {
+var product_info = [];
+var targetImg = (function() {'use strict';
+                             document.getElementById().src;})();
+    var targetTitle = function() {'use strict';
+                                  document.getElementById().value;};
+    var targetPrice = function() {'use strict';
+                                  document.getElementsByClassName().value;};
+  if (document(location.hostname) === 'www.amazon.com') {
 		targetImg('main-image');
 		targetTitle('btAsinTitle');
 		targetPrice('priceLarge');
-	} else if (document(location.hostname) == 'www.etsy.com') {
+	} else if (document(location.hostname) === 'www.etsy.com') {
 		targetImg('fullimage_link1 img');
 		targetTitle('item-title');
-	    targetPrice('v2:nth-child(1)' 'currency-value');
+		targetPrice('v2:nth-child(1) currency-value');
 	} else {
-		targetImg('fullimage_link1' 'img'); //assumes Jcrew and uses associated class & id selectors
+		targetImg('fullimage_link1 img'); //assumes Jcrew and uses associated class & id selectors
 		targetTitle('item-title');
-		targetPrice('v2:nth-child(1)' 'currency-value');
+		targetPrice('v2:nth-child(1) currency-value');
 	} 
-}
+
+product_info.push(targetImg);
+product_info.push(targetTitle);
+product_info.push(targetPrice);
+  return product_info;
