0804c83e474f568a62d9cbb3de35eba279f94e5a
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-21 19:27:37 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -15,11 +15,11 @@ var state = '';
 		if (el_class !== null) {
 			state = el_class.value;}
                                   };
-  if (document(location.hostname) === 'www.amazon.com') {
+  if (document.write(location.hostname) === 'www.amazon.com') {
 		targetImg('main-image');
 		targetTitle('btAsinTitle');
 		targetPrice('priceLarge');
-	} else if (document(location.hostname) === 'www.etsy.com') {
+	} else if (document.write(location.hostname) === 'www.etsy.com') {
 		targetImg('fullimage_link1 img');
 		targetTitle('item-title');
 		targetPrice('v2:nth-child(1) currency-value');
