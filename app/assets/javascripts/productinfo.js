41b0d2ea09fc40ad50ae3b2bcbbfc5d414467a52
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-22 10:29:05 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -2,19 +2,19 @@ var product_info = [];
 if (window.location.hostname === 'www.amazon.com') {
  var targetImg = document.querySelector('#holderMainImage','#main-image');
  var targetTitle = document.getElementById('btAsinTitle').innerHTML;
- var targetPrice = document.getElementById('actualPriceValue').innerHTML;}
-  product_info.push(targetImg)
-  product_info.push(targetTitle)
-  product_info.push(targetPrice)
+ var targetPrice = document.getElementById('actualPriceValue').innerHTML;
+  product_info.push(targetImg);
+  product_info.push(targetTitle);
+  product_info.push(targetPrice);}
 else if (window.location.hostname === 'www.etsy.com') {
  var targetImg = document.querySelector('#fullimage_link1','img').innerHTML;
  var targetTitle = document.getElementById('item-title').innerHTML;
- var targetPrice = document.querySelector('.item-amount', '.currency-value').innerHTML;}
-  product_info.push(targetImg)
-  product_info.push(targetTitle)
-  product_info.push(targetPrice)
+ var targetPrice = document.querySelector('.item-amount', '.currency-value').innerHTML;
+  product_info.push(targetImg);
+  product_info.push(targetTitle);
+  product_info.push(targetPrice);}
 else if (window.location.hostname === 'www.jcrew.com') {
- var targetTitle = document.querySelector('#pdp-title', 'h1').innerHTML;}
-  product_info.push(targetTitle)
-else (console.log("We're sorry you must be shopping an unsupported site"))
+ var targetTitle = document.querySelector('#pdp-title', 'h1').innerHTML;
+  product_info.push(targetTitle);}
+else (console.log("We're sorry you must be shopping an unsupported site"));
 console.log(targetTitle); 
