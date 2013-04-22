536ac23f65d04de6a2537c81cf37d2f8d703a338
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-22 02:49:48 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -3,7 +3,7 @@ var targetImg = document.querySelector('#holderMainImage','#main-image');
 var targetTitle = document.getElementById('btAsinTitle').innerHTML;
 var targetPrice = document.getElementById('actualPriceValue').innerHTML;}
 else if (window.location.hostname === 'www.etsy.com') {
-var targetImg = document.querySelector('#fullimage_link1','img');
+var targetImg = document.querySelector('#fullimage_link1','img').innerHTML;
 var targetTitle = document.getElementById('item-title').innerHTML;
 var targetPrice = document.querySelector('.item-amount', '.currency-value').innerHTML;}  
 console.log(targetTitle, targetPrice, targetImg); 
