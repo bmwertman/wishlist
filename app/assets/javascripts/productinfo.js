ee4abcc7c41c973e9d371238a631be7489658463
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-21 22:41:01 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -1,28 +1,12 @@
 var product_info = [];
 var baseurl = "media/images/";
-	function target_img(id) {'use strict';
+	function targetImg(id) {'use strict';
 		document.getElementById(id ).src.indexOf( baseurl+"selection-off.png" ) >= 0;}
-	function target_title(id) {'use strict';
+	function targetTitle(id) {'use strict';
 		document.getElementById(id).value;}
-	function target_price(id) {'use strict';
+	function targetPrice(id) {'use strict';
 		document.getElementsByClassName(id).value;}
-  if (window.location.hostname === 'www.amazon.com') {
-		target_img('main-image');
-		target_title('btAsinTitle');
-		target_price('priceLarge');
-	} else if (window.location.hostname === 'www.etsy.com') {
-		target_img('fullimage_link1 img');
-		target_title('item-title');
-		target_price('v2:nth-child(1) currency-value');
-	} else {
-		target_img('fullimage_link1 img'); //assumes Jcrew and uses associated class & id selectors
-		target_title('item-title');
-		target_price('v2:nth-child(1) currency-value');
-	} 
-var targetImg = target_img();
-var targetTitle = target_title();
-var targetPrice = target_price();
-product_info.push(targetImg);
-product_info.push(targetTitle);
-product_info.push(targetPrice);
+product_info.push(targetImg('main-image'));
+product_info.push(targetTitle('btAsinTitle'));
+product_info.push(targetPrice('priceLarge'));
   window.alert(product_info); 
