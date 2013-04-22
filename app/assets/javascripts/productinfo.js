1089bbf241f647c616a0ffdbe65d3952e125814b
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-22 08:57:26 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -7,6 +7,6 @@ var targetImg = document.querySelector('#fullimage_link1','img').innerHTML;
 var targetTitle = document.getElementById('item-title').innerHTML;
 var targetPrice = document.querySelector('.item-amount', '.currency-value').innerHTML;}
 else if (window.location.hostname === 'www.jcrew.com') {
-var targetTitle = document.getElementsByTagName('h1').innerHTML;}
+var targetTitle = document.querySelector('#pdp-title', 'h1').innerHTML;}
 else (console.log("We're sorry you must be shopping an unsupported site"))
 console.log(targetTitle); 
