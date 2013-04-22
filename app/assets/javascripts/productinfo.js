d340698977b826f04cbecc0f1ac9c060a761ed8f
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-22 00:35:59 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -1,4 +1,4 @@
-var targetImg = document.getElementById('#main-mage').src;
+var targetImg = document.getElementById('#main-mage').innerHTML;
 var targetTitle = document.getElementById('btAsinTitle').innerHTML;
 var targetPrice = document.getElementById('actualPriceValue').innerHTML;
 console.log(targetTitle, targetPrice, targetImg); 
