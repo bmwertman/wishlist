29112f6680de46d93162ff64459ea3f20367de6f
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-22 10:31:06 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -1,5 +1,5 @@
-var product_info = [];
 if (window.location.hostname === 'www.amazon.com') {
+ var product_info = [];
  var targetImg = document.querySelector('#holderMainImage','#main-image');
  var targetTitle = document.getElementById('btAsinTitle').innerHTML;
  var targetPrice = document.getElementById('actualPriceValue').innerHTML;
@@ -7,6 +7,7 @@ if (window.location.hostname === 'www.amazon.com') {
   product_info.push(targetTitle);
   product_info.push(targetPrice);}
 else if (window.location.hostname === 'www.etsy.com') {
+ var product_info = [];
  var targetImg = document.querySelector('#fullimage_link1','img').innerHTML;
  var targetTitle = document.getElementById('item-title').innerHTML;
  var targetPrice = document.querySelector('.item-amount', '.currency-value').innerHTML;
@@ -14,6 +15,7 @@ else if (window.location.hostname === 'www.etsy.com') {
   product_info.push(targetTitle);
   product_info.push(targetPrice);}
 else if (window.location.hostname === 'www.jcrew.com') {
+ var product_info = [];
  var targetTitle = document.querySelector('#pdp-title', 'h1').innerHTML;
   product_info.push(targetTitle);}
 else (console.log("We're sorry you must be shopping an unsupported site"));
