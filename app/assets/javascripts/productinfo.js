b1d57d84f2c050ff43ccd8c3654aa9fc11ff1f3a
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-22 02:34:04 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -5,5 +5,5 @@ var targetPrice = document.getElementById('actualPriceValue').innerHTML;}
 else if (window.location.hostname === 'www.etsy.com') {
 var targetImg = document.querySelector('#fullimage_link1','img');
 var targetTitle = document.getElementById('item-title').innerHTML;
-var targetPrice = document.querySelector('.v2:nth-child(1)''.currency-value');}  
+var targetPrice = document.querySelector('.v2:nth-child(1)', '.currency-value');}  
 console.log(targetTitle, targetPrice, targetImg); 
