1d12464263b826066e218a51679ea99575458eb8
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-22 02:58:44 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -5,5 +5,10 @@ var targetPrice = document.getElementById('actualPriceValue').innerHTML;}
 else if (window.location.hostname === 'www.etsy.com') {
 var targetImg = document.querySelector('#fullimage_link1','img').innerHTML;
 var targetTitle = document.getElementById('item-title').innerHTML;
-var targetPrice = document.querySelector('.item-amount', '.currency-value').innerHTML;}  
+var targetPrice = document.querySelector('.item-amount', '.currency-value').innerHTML;}
+else if (window.location.hostname === 'www.jcrew.com') {
+ var targetImg = document.getElementById('#mainImg').innerHTML;
+var targetTitle = document.getElementsByTagName('h1').innerHTML;
+var targetPrice = document.querySelector('.pdp-shape:nth-child(1)', '.price).innerHTML';}
+else (console.log("We're sorry you must be shopping an unsupported site"))
 console.log(targetTitle, targetPrice, targetImg); 
