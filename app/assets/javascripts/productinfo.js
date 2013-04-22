3ea147970d5a68e46885d1c2afe1f950f81d0028
Brad Wertman <brad.wertman@leanmeanz.com>
2013-04-22 02:16:48 -0300

    Update productinfo.js

--- app/assets/javascripts/productinfo.js
+++ app/assets/javascripts/productinfo.js
@@ -1,4 +1,9 @@
-var targetImg = document.querySelector('#twister-main-image','#main-image');
+ if (window.location.hostname === 'www.amazon.com') {
+var targetImg = document.querySelector('#holderMainImage','#main-image');
 var targetTitle = document.getElementById('btAsinTitle').innerHTML;
-var targetPrice = document.getElementById('actualPriceValue').innerHTML;
+var targetPrice = document.getElementById('actualPriceValue').innerHTML;}
+else if (window.location.hostname === 'www.etsy.com') {
+var targetImg = document.querySelector('#fullimage_link1','img');
+var targetTitle = document.getElementById('item-title').innerHTML;
+var targetPrice = document.getElementById('v2:nth-child(1)', 'currency-value').innerHTML;}  
 console.log(targetTitle, targetPrice, targetImg); 
